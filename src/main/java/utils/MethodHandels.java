b79dbcc3d87445430663f8ebf2d187f2b6d559b7
user <user@Khaleds-iMac.local>
2024-07-14 18:02:27 +0300

    changes

--- src/main/java/utils/MethodHandels.java
+++ src/main/java/utils/MethodHandels.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 
 public class MethodHandels{
 
-    WebDriver driver ;
+    protected WebDriver driver ;
     WebDriverWait wait ;
     public MethodHandels (WebDriver driver){
         this.driver = driver ;
@@ -20,14 +20,20 @@ public class MethodHandels{
         return driver.findElement(locator) ;
     }
     public void waitElement( By locator ){
-        wait = new WebDriverWait(driver , Duration.ofSeconds(5) );
+        wait = new WebDriverWait(driver , Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOf(webElement(locator))) ;
 
     }
     public void click(By locator){
         waitElement(locator);
-        driver.findElement(locator).click();
+        webElement(locator).click();
     }
+
+    public void sendKeys( By locator , String text){
+        waitElement(locator);
+        webElement(locator).sendKeys(text);
+    }
+
 
 
 
